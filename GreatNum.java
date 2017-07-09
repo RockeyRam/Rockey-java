f79d23cd603c587280004c19b0c3245a9ae74f1e
RockeyRam <rockeyramsaran@gmail.com>
2017-07-08 23:55:46 -0400

    Add files via upload

---
+++ GreatNum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class GreatNum
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		System.out.println("Enter Three numbers");
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		if(a>b&&a>c)
+		{
+			System.out.println("The greatest number is "+a);
+		}
+		else if(b>a&&b>c)
+		{
+			System.out.println("The greatest number is "+b);
+		}
+		else if(c>a&&c>b)
+		{
+			System.out.println("The greatest number is "+c);
+		}
+	}
+}
