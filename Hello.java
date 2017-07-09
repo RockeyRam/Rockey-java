217bdf4c447a2152cb8fefed0d55a0af11747c92
RockeyRam <rockeyramsaran@gmail.com>
2017-07-09 00:25:15 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import java.io.*;
+
+class Hello
+
+{
+  
+	public static void main(String args[])
+  
+	{
+    
+		int i;
+    
+		String a="Hello"
+;    
+		for(i=1;i<=5;i++)
+    
+		{
+      
+			System.out.println(a);
+    
+		}
+  
+	}
+
+}
