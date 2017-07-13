9aa04b921aa90b23a9f55cd8a1d110fd8328fd44
RockeyRam <rockeyramsaran@gmail.com>
2017-07-13 09:09:53 +0530

    Create OddIn.java

---
+++ OddIn.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class OddIn
+{
+  int a,b,i;
+  Scanner S=new Scanner(System.in);
+  System.out.println("Enter Two Intervels ");
+  a=s.nextInt();
+  b=s.nextInt();
+  for(i=a;i<=b;i++)
+  {
+     if(i%2!=0)
+    {
+      Systme.out.println(" "+i);
+    }
+  }
+ 
+  }
+}
