15f17450c036f469857a55bc78f278a92a2c3e41
RockeyRam <rockeyramsaran@gmail.com>
2017-09-14 14:53:52 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class Ascii 
+{
+	public static void main(String args[])
+	{
+		int a=0;
+		System.out.println("The Ascii Values are");
+		while(a<=250)
+		{
+			System.out.println(a);
+			a++;
+		}
+	}
+}
